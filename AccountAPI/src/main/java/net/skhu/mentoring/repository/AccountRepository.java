2f662d2eee1d07d466ec6d57f9186ceac9cbb2a1
tails5555 <tails5555@naver.com>
2018-09-08 15:59:08 +0900

    Create AccountAPI - AdminService.java 관리자가 회원 전체를 검색할 수 있는 기능을 추가하였음.

--- AccountAPI/src/main/java/net/skhu/mentoring/repository/AccountRepository.java
+++ AccountAPI/src/main/java/net/skhu/mentoring/repository/AccountRepository.java
@@ -1,12 +1,80 @@
 package net.skhu.mentoring.repository;
 
 import net.skhu.mentoring.domain.Account;
+import net.skhu.mentoring.enumeration.UserType;
+import net.skhu.mentoring.model.AccountPagination;
+import net.skhu.mentoring.model.OptionModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends AccountBaseRepository<Account> {
+    List<Integer> sizeBy = Arrays.asList(4, 6, 8, 10, 12, 14, 16, 18, 20);
+    List<OptionModel> searchBy = Arrays.asList(new OptionModel(1, "회원 아이디 포함 검색"), new OptionModel(2, "회원 이름 포함 검색"), new OptionModel(3, "회원 주 학과 포함 검색"));
+    List<OptionModel> orderBy = Arrays.asList(new OptionModel(1, "회원 가입 순"), new OptionModel(2, "회원 가입 역순"), new OptionModel(3, "회원 이름 순"), new OptionModel(4, "회원 이름 역순"), new OptionModel(5, "회원 아이디 순"), new OptionModel(6, "회원 아이디 역순"));
+
+    Sort[] sort = {
+            new Sort(Sort.Direction.ASC, "id"),
+            new Sort(Sort.Direction.DESC, "id"),
+            new Sort(Sort.Direction.ASC, "name"),
+            new Sort(Sort.Direction.DESC, "name"),
+            new Sort(Sort.Direction.ASC, "identity"),
+            new Sort(Sort.Direction.DESC, "identity")
+    };
+
+   default List<Account> findAll(AccountPagination accountPagination){
+        Pageable pageable = new PageRequest(accountPagination.getPg() - 1, accountPagination.getSz(), sort[(accountPagination.getOb() != 0) ? accountPagination.getOb() - 1 : accountPagination.getOb()]);
+        Page<Account> page;
+        String type = null;
+
+        switch(accountPagination.getTb()){
+            case 1 :
+                type = UserType.STUDENT;
+                break;
+            case 2 :
+                type = UserType.PROFESSOR;
+                break;
+            case 3 :
+                type = UserType.EMPLOYEE;
+                break;
+        }
+
+        String searchText = accountPagination.getSt();
+        switch(accountPagination.getSb()){
+            case 1 :
+                page = (type == null) ? this.findByIdentityContains(searchText, pageable) : this.findByTypeAndIdentityContains(type, searchText, pageable);
+                break;
+            case 2 :
+                page = (type == null) ? this.findByNameContains(searchText, pageable) : this.findByTypeAndNameContains(type, searchText, pageable);
+                break;
+            case 3 :
+                page = (type == null) ? this.findByDepartmentNameContains(searchText, pageable) : this.findByTypeAndDepartmentNameContains(type, searchText, pageable);
+                break;
+            default :
+                page = (type == null) ? this.findAll(pageable) : this.findByType(type, pageable);
+                break;
+        }
+        accountPagination.setRequestCount(page.getTotalElements());
+        return page.getContent();
+    }
+
+    Page<Account> findAll(Pageable pageable);
+    Page<Account> findByIdentityContains(String identity, Pageable pageable);
+    Page<Account> findByNameContains(String name, Pageable pageable);
+    Page<Account> findByDepartmentNameContains(String departmentName, Pageable pageable);
+
+    Page<Account> findByType(String type, Pageable pageable);
+    Page<Account> findByTypeAndIdentityContains(String type, String identity, Pageable pageable);
+    Page<Account> findByTypeAndNameContains(String type, String name, Pageable pageable);
+    Page<Account> findByTypeAndDepartmentNameContains(String type, String departmentName, Pageable pageable);
+
     Optional<Account> findByNameAndEmail(String name, String email);
     Optional<Account> findByIdentity(String identity);
     boolean existsByIdentity(String identity);
---
+++ AccountAPI/src/main/java/net/skhu/mentoring/service/implement_objects/AdminServiceImpl.java
@@ -0,0 +1,103 @@
+package net.skhu.mentoring.service.implement_objects;
+
+import net.skhu.mentoring.component.JwtTokenProvider;
+import net.skhu.mentoring.domain.Account;
+import net.skhu.mentoring.domain.Employee;
+import net.skhu.mentoring.domain.Student;
+import net.skhu.mentoring.enumeration.StudentStatus;
+import net.skhu.mentoring.enumeration.UserType;
+import net.skhu.mentoring.model.AccountPagination;
+import net.skhu.mentoring.repository.AccountRepository;
+import net.skhu.mentoring.repository.AvailableTimeRepository;
+import net.skhu.mentoring.repository.DepartmentRepository;
+import net.skhu.mentoring.repository.EmployeeRepository;
+import net.skhu.mentoring.repository.ProfessorRepository;
+import net.skhu.mentoring.repository.StudentRepository;
+import net.skhu.mentoring.service.interfaces.AdminService;
+import net.skhu.mentoring.vo.BriefAccountVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private ProfessorRepository professorRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    private boolean tokenValidation(final Principal principal, final HttpServletRequest request) {
+        String jwtToken;
+        String tokenLoginId;
+        boolean hasAbleAccess;
+        Account account = accountRepository.findByIdentity(principal.getName()).get();
+        if(account.getType().equals(UserType.EMPLOYEE) || account.getType().equals(UserType.PROFESSOR)) {
+            hasAbleAccess = true;
+
+        } else {
+            Student student = studentRepository.findByIdentity(principal.getName()).get();
+            hasAbleAccess = student.getHasChairman();
+        }
+        if (principal != null && hasAbleAccess) {
+            jwtToken = jwtTokenProvider.resolveToken(request);
+            tokenLoginId = jwtTokenProvider.getUsername(jwtToken);
+        } else {
+            return false;
+        }
+        return principal.getName().equals(tokenLoginId);
+    }
+
+
+    @Override
+    public List<BriefAccountVO> fetchAccountListWithPagination(final Principal principal, final HttpServletRequest request, final AccountPagination accountPagination) {
+        if(!this.tokenValidation(principal, request)) return null;
+        List<Account> accounts = accountRepository.findAll(accountPagination);
+        return accounts.stream()
+                .map(account -> {
+                    switch(account.getType()){
+                        case UserType.STUDENT :
+                            Student student = studentRepository.findByIdentity(account.getIdentity()).get();
+                            String stdType = "";
+                            switch(student.getStatus()){
+                                case NORMAL :
+                                    stdType = student.getHasChairman() ? "학생회장" : "일반학생";
+                                    break;
+                                case MENTI :
+                                    stdType = student.getHasChairman() ? "학생회장 + 멘티" : "멘티학생";
+                                    break;
+                                case MENTO :
+                                    stdType = student.getHasChairman() ? "학생회장 + 멘토" : "멘토학생";
+                                    break;
+                            }
+                            return BriefAccountVO.builtToAccountVO(account, stdType, account.getDepartment().getName());
+                        case UserType.PROFESSOR :
+                            return BriefAccountVO.builtToAccountVO(account, "교수", account.getDepartment().getName());
+                        case UserType.EMPLOYEE :
+                            Employee employee = employeeRepository.findByIdentity(account.getIdentity()).get();
+                            List<String> depts = employee.getDepartments().stream()
+                                    .map(dept -> dept.getName())
+                                    .collect(Collectors.toList());
+                            return BriefAccountVO.builtToAccountVO(account, "직원", String.join(", ", depts));
+                        default :
+                            return BriefAccountVO.builtToAccountVO(account, "-", "-");
+                    }
+                }).collect(Collectors.toList());
+    }
+}
