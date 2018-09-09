a5ee5c936d0ec73b3034bc1342572194702358b0
tails5555 <tails5555@naver.com>
2018-09-10 00:22:52 +0900

    Create AccountAPI - ExcelUploadService.java 관리자가 엑셀을 이용하여 신입생, 편입생을 추가할 수 있는 기능 구현 완료.

--- AccountAPI/src/main/java/net/skhu/mentoring/controller/AdminRestController.java
+++ AccountAPI/src/main/java/net/skhu/mentoring/controller/AdminRestController.java
@@ -2,7 +2,9 @@ package net.skhu.mentoring.controller;
 
 import net.skhu.mentoring.model.AccountPagination;
 import net.skhu.mentoring.service.interfaces.AdminService;
+import net.skhu.mentoring.service.interfaces.ExcelUploadService;
 import net.skhu.mentoring.vo.FindResultVO;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +12,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.security.Principal;
 
 @RestController
@@ -23,6 +32,9 @@ import java.security.Principal;
 public class AdminRestController {
     @Autowired
     private AdminService adminService;
+
+    @Autowired
+    private ExcelUploadService excelUploadService;
 
     @GetMapping("account/list")
     public ResponseEntity<?> fetchAccountListWithPagination(Principal principal, HttpServletRequest request, AccountPagination accountPagination){
@@ -54,5 +66,23 @@ public class AdminRestController {
     @GetMapping("account/options/order_by")
     public ResponseEntity<?> fetchOrderByOptions(Principal principal, HttpServletRequest request){
         return ResponseEntity.ok(adminService.getOrderByModel(principal, request));
+    }
+
+    @PostMapping(value="account/excel/upload", consumes = {"multipart/form-data"})
+    public ResponseEntity<?> executeExcelUploadSaving(Principal principal, HttpServletRequest request, @RequestPart("file") MultipartFile multipartFile) throws IOException {
+        if(multipartFile.getSize() <= 0)
+            return new ResponseEntity<String>("파일 형식이 잘 못 되었습니다.", HttpStatus.NON_AUTHORITATIVE_INFORMATION);
+        return excelUploadService.executeExcelUploading(principal, request, multipartFile.getInputStream());
+    }
+
+    @GetMapping("account/excel/sample_document")
+    public void executeExcelSampleFormDownload(Principal principal, HttpServletRequest request, HttpServletResponse response) throws Exception{
+        XSSFWorkbook workbook=excelUploadService.fetchSampleExcelFile();
+        String fileName = URLEncoder.encode("신입생_추가_폼.xlsx", "UTF-8");
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment;filename="+fileName+";");
+        try (BufferedOutputStream output = new BufferedOutputStream(response.getOutputStream())) {
+            workbook.write(output);
+        }
     }
 }
