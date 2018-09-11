5e2d83ec1ff19dad51cd3f345e87872aa7c116e2
wkdtndgns <wkdtndgns@naver.com>
2018-09-11 23:47:06 +0900

    repository 작업

---
+++ MentoAPI/src/main/java/net/skhu/mentoring/repository/TeamRepository.java
@@ -0,0 +1,10 @@
+package net.skhu.mentoring.repository;
+
+import net.skhu.mentoring.domain.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
