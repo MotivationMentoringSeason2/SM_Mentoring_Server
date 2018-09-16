f28d608c151d3ccc3da3efdfd774f044cd95373b
tails5555 <tails5555@naver.com>
2018-09-16 16:27:01 +0900

    Update NoticeAPI - NoticeCommentService.java 댓글 관련 기능을 구현하기 위한 Service 클래스 최종 생성 완료.

--- NoticeAPI/src/main/java/net/skhu/mentoring/repository/CommentRepository.java
+++ NoticeAPI/src/main/java/net/skhu/mentoring/repository/CommentRepository.java
@@ -4,6 +4,13 @@ import net.skhu.mentoring.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+    boolean existsByWriter(String writer);
+    boolean existsByIdIn(List<Long> ids);
+    void deleteByWriter(String writer);
+    void deleteByIdIn(List<Long> id);
 }
