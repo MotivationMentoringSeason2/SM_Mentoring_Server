f231ace79183064d5f7374751f5a44edffbf689c
tails5555 <tails5555@naver.com>
2018-09-17 15:19:33 +0900

    Create NoticeAPI - SurveyService.java 설문조사 사이트 올리는 기능에 대해 Service 단위 구현 완료. REST API 까지 구축하고 난 후에 MentoAPI 측으로 넘어가겠음. Alert 는 실제로 Web App 까지 구현을 하고 구성해볼 생각임.

--- NoticeAPI/src/main/java/net/skhu/mentoring/repository/SurveyRepository.java
+++ NoticeAPI/src/main/java/net/skhu/mentoring/repository/SurveyRepository.java
@@ -1,9 +1,14 @@
 package net.skhu.mentoring.repository;
 
 import net.skhu.mentoring.domain.Survey;
+import net.skhu.mentoring.enumeration.SurveyType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
+    List<Survey> findByTypeIn(List<SurveyType> type);
+    void deleteByIdIn(List<Long> id);
 }
