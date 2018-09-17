4baed51088599a08c3002cf507b4cbf3b1d459bb
wkdtndgns <wkdtndgns@naver.com>
2018-09-18 03:03:45 +0900

    내가 봤을떄 NoticePostBriefVo에서 api 바꾸는 거 같은데 바꿨는데도 안 바뀌네 확인 한번 해주라

--- NoticeAPI/src/main/java/net/skhu/mentoring/service/integrate_objects/NoticeIntegrateServiceImpl.java
+++ NoticeAPI/src/main/java/net/skhu/mentoring/service/integrate_objects/NoticeIntegrateServiceImpl.java
@@ -47,6 +47,7 @@ public class NoticeIntegrateServiceImpl implements NoticeIntegrateService {
     @Override
     public Type fetchTypeById(final Long typeId) {
         Optional<Type> type = typeRepository.findById(typeId);
+        System.out.println(type.get());
         if(type.isPresent())
             return type.get();
         else return null;
@@ -57,6 +58,7 @@ public class NoticeIntegrateServiceImpl implements NoticeIntegrateService {
         return postRepository.findAll(noticePagination).stream()
                 .map(post -> NoticePostBriefVO.builtToVO(post))
                 .collect(Collectors.toList());
+
     }
 
     @Override
